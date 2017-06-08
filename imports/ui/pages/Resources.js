adcec18f7d55cc13accdaad301d8744a21696aec
lizihan <lizihan@umich.edu>
2017-06-08 00:18:16 -0700

    yo

--- imports/ui/pages/Resources.js
+++ imports/ui/pages/Resources.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
-import ReactPDF from 'react-pdf';
-
-const pdf = require('file-loader!./Zihan-Li-Reprot.pdf')
-
 class Resources extends React.Component {
     render() {
         return (
             <div className="Resources">
-                <ReactPDF
-                    file={ pdf }
-                />
             </div>
         );
     }
