87e3d7438f92aa132dcb6365d4775848f9b06a00
lizihan <lizihan@umich.edu>
2017-06-15 00:35:15 -0700

    success

--- imports/ui/pages/Newsletter.js
+++ imports/ui/pages/Newsletter.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 const Newsletter = () => (
   <div className="Newsletter">
+    <embed src="http://localhost:3000/files/news.html" height="3450" type='text/html'></embed>
+    {/*
+    <embed src="http://localhost:3000/files/news.pdf" height="675" type='application/pdf'></embed>
+    <iframe scrolling="yes" src="https://docs.google.com/document/d/1ntLN9IyxFvexjBUm-KUQj2IQ29C9MiXG-1WosUbsDpU/pub?embedded=true"></iframe>
+  */}
 	</div>
 );
 
