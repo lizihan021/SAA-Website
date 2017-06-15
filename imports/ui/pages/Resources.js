10d08415d6bef994ad99db0cefd20386cc6df768
lizihan <lizihan@umich.edu>
2017-06-15 12:31:14 -0700

    yo

--- imports/ui/pages/Resources.js
+++ imports/ui/pages/Resources.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { Link } from 'react-router';
+import { Table } from 'react-bootstrap';
 class Resources extends React.Component {
     render() {
         return (
             <div className="Resources">
             	<h2 className="page-header">2014 DD Students:</h2>
-
+            	<Table striped hover>
+						    <thead>
+						      <tr>
+						        <th colSpan="3">file name</th>
+						        <th>publish date</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						      <tr>
+						        <td colSpan="3"><Link to="/home">Lorem ipsum dolor sit amet, consectetur adipiscing</Link></td>
+						        <td>6/15/2017</td>
+						      </tr>
+						      <tr>
+						        <td colSpan="3"><Link to="/home">Lorem ipsum dolor sit amet, consectetur adipiscing</Link></td>
+						        <td>6/15/2017</td>
+						      </tr>
+						      <tr>
+						        <td colSpan="3"><Link to="/home">Lorem ipsum dolor sit amet, consectetur adipiscing</Link></td>
+						        <td>6/15/2017</td>
+						      </tr>
+						    </tbody>
+						  </Table>
             </div>
         );
     }
---
+++ imports/ui/pages/ViewResources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotFound from './NotFound';
+import PropTypes from 'prop-types';
+
+const ViewResources = () => {
+  return (
+    <div className="ViewResources">
+      <p> {//this.props.params._id
+      } </p>
+    </div>
+  );
+};
+
+export default ViewResources;
